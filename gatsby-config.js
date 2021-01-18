18b29c4e2e2799e3993cefab87a180106715ffac
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-01-19 02:02:16 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,19 +6,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
-  },
+
   plugins: [
+    "gatsby-plugin-typescript",
     `gatsby-plugin-material-ui`,
     {
       resolve: "gatsby-plugin-firebase",
