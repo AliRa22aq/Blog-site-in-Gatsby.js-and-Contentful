e1ed9e10dabcdc2849afbdb0bfd9f75c0df34fd1
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2021-01-19 00:59:21 +0500

    Update googleSignIn.tsx

--- src/components/googleSignIn.tsx
+++ src/components/googleSignIn.tsx
@@ -1,5 +1,18 @@
 import firebase from 'firebase';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAfmjcK0SsuChX_cYRwCJq45tsHCmctq28",
+    authDomain: "blog-e7108.firebaseapp.com",
+    projectId: "blog-e7108",
+    storageBucket: "blog-e7108.appspot.com",
+    messagingSenderId: "538281889243",
+    appId: "1:538281889243:web:96123a606aa17fb559ebfe"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+
 
 export const auth = firebase.auth();
 
